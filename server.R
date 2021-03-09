f6c4da2a5d5ca7b171994244cf36dcea970996bf
matswinkels <m_sliwakowski@wp.pl>
2021-03-09 13:52:02 +0100

    file input functionality

--- server.R
+++ server.R
@@ -1,5 +1,65 @@
 library(shiny)
 
 shinyServer(function(input, output, session) {
+  
+  dataset <- reactiveValues(
+    original = NULL
+  )
+  
+  observeEvent(input$input.file, {
+    # Pierwsze wczytanie danych
+    if (!is.null(input$input.file)) {
+      tryCatch({
+        dataset$original <- read.csv(
+          file = input$input.file$datapath,
+          header = input$header,
+          stringsAsFactors = input$stringAsFactors,
+          sep = input$sep,
+          dec = input$dec)
+        
+      },
+      warning = function(warn){
+        showNotification(paste0(warn), type = 'warning', duration = 10, closeButton = TRUE)
+      },
+      error = function(err){
+        showNotification(paste0(err), type = 'err', duration = 10, closeButton = TRUE)
+      }
+      )
+    }
+  })
+  
+  updateValues <- reactive({
+    # Aktualizuje wartosi zmiennych (TO DO)
+    return (NULL)
+  })
+  
+  observeEvent(input$load.again, {
+    # Ponowne zaladowanie danych
+    if (!is.null(input$input.file)) {
+      tryCatch({
+        dataset$original <- read.csv(
+          file = input$input.file$datapath,
+          header = input$header,
+          stringsAsFactors = input$stringAsFactors,
+          sep = input$sep,
+          dec = input$dec)
+  
+      },
+      warning = function(warn){
+        showNotification(paste0(warn), type = 'warning', duration = 8, closeButton = TRUE)
+      },
+      error = function(err){
+        showNotification(paste0(err), type = 'err', duration = 8, closeButton = TRUE)
+      }
+      )
+    }
+  })
+  
+  output$render.table <- DT::renderDT({
+    # Renderuje tabele w zakladce 'eksploracja'
+    if (is.null(input$input.file))
+      return (NULL)
+    dataset$original
+  })
 
 })
--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(
       tags$li(
         a(
           icon('github', 'fa-2x'),
-          href = 'https://github.com/matswinkels/EDA-App-Project',
+          href = 'https://github.com/matswinkels/EDA-App-Project-2',
           target = '_blank'
         ),
         class = 'dropdown'
@@ -27,11 +27,60 @@ shinyUI(
           type = 'text/css',
           href = 'style.css'
         )
-      )
+      ),
+      
+      fileInput(
+        inputId = 'input.file',
+        label = 'Wczytaj plik .csv',
+        accept = c(
+          "text/csv",
+          "text/comma-separated-values,text/plain",
+          ".csv"
+        ),
+        multiple = FALSE,
+        placeholder = 'Nie wybrano pliku',
+        buttonLabel = 'Przegladaj'
+      ),
+      
+      checkboxInput(
+        inputId = 'header', 
+        label = 'Header', 
+        value = TRUE
+      ),
+      
+      checkboxInput(
+        inputId = 'stringAsFactors',
+        label = 'String as factors',
+        value = FALSE
+      ),
+      
+      radioButtons(
+        inputId = 'sep',
+        label = 'Separator',
+        choices = c(
+          przecinek = ',',
+          srednik = ';',
+          tab = '\t',
+          spacja = ' '
+        ),
+        selected = ','
+      ),
+      
+      radioButtons(
+        inputId = 'dec',
+        label = 'Separator dziesietny',
+        choices = c(
+          kropka = '.',
+          przecinek = ','
+        ),
+        selected = '.'
+      ),
+      
+      actionButton('load.again', 'Zaladuj ponownie', width = 180)
     ),
     
     shinydashboard::dashboardBody(
-      
+      DT::DTOutput('render.table')
     )
     
   )
